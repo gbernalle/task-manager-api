c526d40421cc37865e32f1f1f3d654ce636986ba
gbernalle <gabrielbbmatozinhos@gmail.com>
2026-08-19 14:45:20 -0300

    feat: add PATCH and DELETE endpoints and exception handling
    - Implement PATCH and DELETE routes in TaskController
    - Add ResourceNotFoundException for invalid IDs
    - Refactor TaskService to use Java Streams and Lambdas

--- src/main/java/com/gabrielbernalle/taskmanager/controller/TaskController.java
+++ src/main/java/com/gabrielbernalle/taskmanager/controller/TaskController.java
@@ -3,17 +3,23 @@ package com.gabrielbernalle.taskmanager.controller;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gabrielbernalle.taskmanager.entities.Task;
 import com.gabrielbernalle.taskmanager.service.TaskService;
 
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
 	
 	private final TaskService taskService;
@@ -23,35 +29,46 @@ public class TaskController {
 	}
 	
 	
-	@GetMapping("/tasks")
+	@GetMapping
 	public List<Task> allTasks(){
 		return taskService.getBd();
 	}
 	
-	@PostMapping("/tasks")
+	@PostMapping
 	public Task newTask(@RequestBody Task newTask) {
 		taskService.addTask(newTask);
-		return taskService.getById(newTask);
+		return taskService.findById(newTask.getId());
 	}
 	
-	@GetMapping("/tasks/{id}")
+	@GetMapping("{id}")
 	public Task oneTask(@PathVariable UUID id) {
 		return taskService.findById(id);
 	}
 	
-	@PutMapping("/tasks/{id}")
+	@PutMapping("{id}")
 	public Task replaceTask(@RequestBody Task newTask, @PathVariable UUID id) {
 		Task existingTask = taskService.findById(id);
 		
-		if (existingTask != null) {
-			existingTask.setTitle(newTask.getTitle());
-			existingTask.setDescription(newTask.getDescription());
-			return existingTask;
-		} else {
-			newTask.setId(id);
-			taskService.addTask(newTask);
-			return newTask;
-		}
+		existingTask.setTitle(newTask.getTitle());
+		existingTask.setDescription(newTask.getDescription());
+		
+		return existingTask;
+	}
+	
+	@DeleteMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteTask(@PathVariable UUID id) {
+		taskService.deleteById(id);		
+	}
+	
+	@PatchMapping("{id}")
+	public Task updateTask(@RequestBody Task updateStatus, @PathVariable UUID id) {
+		Task existingTask = taskService.findById(id);
+		
+		existingTask.setStatus(updateStatus.getStatus());
+		
+		return existingTask;
+		
 	}
 	
 }
--- src/main/java/com/gabrielbernalle/taskmanager/service/TaskService.java
+++ src/main/java/com/gabrielbernalle/taskmanager/service/TaskService.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 import org.springframework.stereotype.Service;
 
 import com.gabrielbernalle.taskmanager.entities.Task;
+import com.gabrielbernalle.taskmanager.exceptions.ResourceNotFoundException;
 
 @Service
 public class TaskService {
@@ -25,22 +26,15 @@ public class TaskService {
 		return bd;
 	}
 	
-	public Task getById(Task task) {
-		for (Task tk : bd) {
-			if(tk.getId().equals(task.getId())) {
-				return tk;
-			}
-		}
-		return null;
+	public Task findById(UUID id) {
+		return bd.stream()
+			.filter(tk -> tk.getId().equals(id))
+			.findFirst()
+			.orElseThrow(()-> new ResourceNotFoundException("Task not found with ID: " + id));
 	}
 	
-	public Task findById(UUID id) {
-		for (Task tk : bd) {
-			if(tk.getId().equals(id)) {
-				return tk;
-			}
-		}
-		return null;
+	public void deleteById(UUID id) {
+		bd.removeIf(tk -> tk.getId().equals(id));
 	}
 
 	public void addTask(Task task) {
